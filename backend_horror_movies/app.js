c200c829d3ea28e4a99caa4bf6609ae7d3397462
zazimov <zalmanazimov@pursuit.org>
2023-08-03 13:05:37 -0400

    First commit

---
+++ backend_horror_movies/app.js
@@ -0,0 +1,22 @@
+const cors = require("cors");
+const express = require("express");
+const morgan = require("morgan");
+
+const movieController = require("./controller/movieController");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan());
+app.use("/movies", movieController);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to FormidulosusWiki");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
+
+module.exports = app;
