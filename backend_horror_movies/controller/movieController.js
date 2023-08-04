a72de9a5c41e97327aa20ab315f76d5bddab60e8
davep80 <davidpaq1@gmail.com>
2023-08-03 22:08:55 -0400

    post delete validations

--- backend_horror_movies/controller/movieController.js
+++ backend_horror_movies/controller/movieController.js
@@ -1,19 +1,39 @@
 const express = require("express");
 const router = express.Router();
-const {getMovies, getMoviesByTD} = require('../queries/movies');
+const { getMovies, getMoviesByTD, getMoviesById, addRow, deleteRow, } = require('../queries/movies');
+const { checkTD, checkId, checkPost } = require('../validations/crudValidations')
 
-router.get("/", async(req, res, next) => {
+router.get("/", checkTD, async (req, res, next) => {
   if (req.query.title) {
-  const movies = await getMoviesByTD(req.query.title, req.query.date);
-  if (movies[0]) res.json(movies);
-  else res.status(500).json({err: 'pg error'})
+    const movies = await getMoviesByTD(req.query.title, req.query.date);
+    if (movies[0]) res.json(movies);
+    else res.status(500).json({ err: 'pg error' })
   } else next()
 });
 
-router.get("/", async(req, res) => {
+router.get("/", async (req, res) => {
   const movies = await getMovies();
   if (movies[0]) res.json(movies);
-  else res.status(500).json({err: 'pg error'})
+  else res.status(500).json({ err: 'pg error' })
 });
+
+router.get("/:id", checkId, async (req, res) => {
+  const movie = await getMoviesById(req.params.id);
+  if (movie[0]) res.json(movie);
+  else res.status(500).json({ err: 'pg error' })
+});
+
+router.post('/', checkPost, async (req, res) => {
+  const newRow = await addRow(req.body);
+  if (newRow[0]) res.status(201).json(newRow[0]);
+  else res.status(500).json({ err: 'pg error' })
+})
+
+router.delete('/:id', checkId, async (req, res) => {
+  const id = req.params.id
+  const status = await deleteRow(id);
+  if (status[0]) res.json(status[0]);
+  else res.redirect('/notfound')
+})
 
 module.exports = router;
--- backend_horror_movies/queries/movies.js
+++ backend_horror_movies/queries/movies.js
@@ -1,13 +1,43 @@
 const db = require('../db/dbConfig');
 
 async function getMovies() {
-    const someMovie = await db.any('select * from horrmovies limit 20').then(res => res).catch(e => {throw e})
+    const someMovie = await db.any('select * from horrmovies limit 20').then(res => res).catch(e => { throw e })
     return someMovie
 }
 //get movie by original title and release_date
 async function getMoviesByTD(title, date) {
-    const someMovie = await db.any('select * from horrmovies where original_title = $1 and release_date = $2', [title, date]).then(res => res).catch(e => {throw e})
+    const someMovie = await db.any('select * from horrmovies where original_title = $1 and release_date = $2', [title, date]).then(res => res).catch(e => { throw e })
     return someMovie
 }
+async function getMoviesById(id) {
+    const movie = await db.any('select * from horrmovies where id = $1', id).then(res => res).catch(e => { throw e })
+    return movie
+}
 
-module.exports = { getMovies, getMoviesByTD }
+async function getCollections() {
+    const collections = await db.any(`select distinct collection_name, collection from horrmovies where collection_name is not null 
+    and collection is not null order by collection`).then(res => res).catch(e => { throw e })
+    return collections
+}
+
+async function getCollById(id) {
+    const coll = await db.any('select * from horrmovies where collection = $1', id).then(res => res).catch(e => { throw e })
+    return coll
+}
+
+async function addRow(args) {
+    try {
+        const Row = await db.any(`INSERT INTO horrmovies (${Object.keys(args).join(',')}) 
+        VALUES (${Object.values(args).map(item => { return `'${item}'` }).join(',')}) RETURNING *`);
+        return Row;
+    } catch (e) { return e }
+};
+
+async function deleteRow(args) {
+    try {
+        const Del = await db.any(`DELETE FROM horrmovies where id = ${args} RETURNING *`);
+        return Del;
+    } catch (e) { return e }
+};
+
+module.exports = { getMovies, getMoviesByTD, getMoviesById, getCollections, getCollById, addRow, deleteRow, }
