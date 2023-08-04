52a55fbb3f6ac51cf047e696f1497ff9a9ca3980
zazimov <zalmanazimov@pursuit.org>
2023-08-04 02:18:44 -0400

    finished CRUD, added validations, and additional get routes for title +/- date, substring

--- backend_horror_movies/controller/movieController.js
+++ backend_horror_movies/controller/movieController.js
@@ -3,17 +3,39 @@ const router = express.Router();
 const {
   getMovies,
   getMoviesByTD,
+  getMoviesByTitle,
+  getMoviesBySubstring,
   getMoviesLimit,
   getMoviesById,
   addRow,
   deleteRow,
+  updateRow,
 } = require("../queries/movies");
+
 const {
   checkTD,
+  checkSearch,
   checkId,
   checkNum,
   checkPost,
+  checkPut,
 } = require("../validations/crudValidations");
+
+router.get("/", async (req, res, next) => {
+  if (req.query.title && !req.query.date) {
+    const movies = await getMoviesByTitle(req.query.title);
+    if (movies[0]) res.json(movies);
+    else res.status(500).json({ err: "pg error" });
+  } else next();
+});
+
+router.get("/search", checkSearch, async (req, res, next) => {
+  if (req.query.title) {
+    const movies = await getMoviesBySubstring(req.query.title.trim());
+    if (movies[0]) res.json(movies);
+    else res.status(500).json({ err: "pg error" });
+  } else next();
+});
 
 router.get("/", checkTD, async (req, res, next) => {
   if (req.query.title) {
@@ -47,21 +69,24 @@ router.post("/", checkPost, async (req, res) => {
   else res.status(500).json({ err: "pg error" });
 });
 
-router.get("/", (req, res) => {
-  res.json({ status: "ok" });
-});
-
-router.post("/", checkPost, async (req, res) => {
-  const newRow = await addRow(req.body);
-  if (newRow[0]) res.status(201).json(newRow[0]);
-  else res.status(500).json({ err: "pg error" });
-});
-
 router.delete("/:id", checkId, async (req, res) => {
   const id = req.params.id;
   const status = await deleteRow(id);
   if (status[0]) res.json(status[0]);
   else res.redirect("/notfound");
+});
+
+router.put("/:id", checkId, checkPut, async (req, res) => {
+  try {
+    const updateMovie = await updateRow(req.body, req.params.id);
+    if (updateMovie.length === 0) {
+      res.status(404).json("This movie is not found");
+    } else {
+      res.status(200).json(updateMovie[0]);
+    }
+  } catch (e) {
+    res.status(400).json({ error: e });
+  }
 });
 
 module.exports = router;
