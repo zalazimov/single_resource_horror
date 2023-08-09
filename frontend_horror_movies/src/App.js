cbdcf23fefc68c1644385e4f7a5e65830d63f725
zazimov <zalmanazimov@pursuit.org>
2023-08-09 10:29:35 -0400

    deleted unnecessary code

--- frontend_horror_movies/src/App.js
+++ frontend_horror_movies/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import EditMovie from "./components/EditMovie/EditMovie";
 import Nav from "./components/Nav/Nav";
 import Home from "./components/Home/Home";
 import CreateMovie from "./components/CreateMovie/CreateMovie";
@@ -14,15 +13,14 @@ function App() {
   const Movies = React.lazy(() => import("./components/Movies/Movies"));
   const Movie = React.lazy(() => import("./components/Movie/Movie"));
   const Index = React.lazy(() => import("./components/DBIndex/Index"));
-  const SearchRes = React.lazy(() => import("./components/SearchRes/SearchRes"));
+  const SearchRes = React.lazy(() =>
+    import("./components/SearchRes/SearchRes")
+  );
 
-  const [movie, setMovieById] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState(null);
 
   const movieContextValue = {
-    movie,
-    setMovieById,
     isLoading,
     setIsLoading,
     posterImage,
@@ -31,8 +29,13 @@ function App() {
   };
 
   useEffect(() => {
-    fetchPopularMovies().then(res => { setData(res.data); localStorage.setItem('avgpopularity', JSON.stringify(res.data)) }).catch(e => console.log(e))
-  }, [])
+    fetchPopularMovies()
+      .then((res) => {
+        setData(res.data);
+        localStorage.setItem("avgpopularity", JSON.stringify(res.data));
+      })
+      .catch((e) => console.log(e));
+  }, []);
 
   return (
     <div className="App">
@@ -47,7 +50,6 @@ function App() {
               <Route path="/search" element={<SearchRes />} />
               <Route path="/create" element={<CreateMovie />} />
               <Route path="/index" element={<Index />} />
-              <Route path="/:id" element={<EditMovie />} />
               <Route path="/404" element={<h1>404 Error Not Found</h1>} />
               <Route path="*" element={<h1>404 Error Not Found</h1>} />
             </Routes>
