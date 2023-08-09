7ceacfa0305d7f3bd1f2b8e53ad55139da7a5a9f
davep80 <davidpaq1@gmail.com>
2023-08-08 22:31:44 -0400

    change movie and setMovie on app.js

--- backend_horror_movies/db/dbConfig.js
+++ backend_horror_movies/db/dbConfig.js
@@ -4,9 +4,8 @@ require("dotenv").config();
 const cn = {
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
-  // port: process.env.PG_PORT,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+  user: process.env.PG_USER
   // When testing, comment out 'password' key, but keep in 'port' key. When Deploying, comment out 'port' key, and uncomment 'password' key so that it is active
 };
 
